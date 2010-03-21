2c085acec9e539777066dc42be3edea7ca8d681c
Sami Greenbury <dude@patabugen.co.uk>
2010-03-21 13:44:05 +0000

    made DBase insertion work, added some comments

--- dbase/Main/Result/User.pm
+++ dbase/Main/Result/User.pm
@@ -3,7 +3,7 @@ use base qw/DBIx::Class::Core/;
 __PACKAGE__->table('users');
 __PACKAGE__->add_columns(qw/ user_id user_name /);
 __PACKAGE__->set_primary_key('user_id');
-__PACKAGE__->has_many('trans' => 'dbase::Main::Result::Tran');
+__PACKAGE__->has_many('trans' => 'dbase::Main::Result::Tran', 'tran_owner');
 
 
 1;
--- insert.pl
+++ insert.pl
@@ -6,13 +6,18 @@ use diagnostics;
 
 use Text::CSV;
 use dbase::Main;
+use Digest::MD5;
 
+## Settings
 my $file = 'statements/2010-feb.csv';
 my $fileStart = 0;
+
+## Library Objects I'll be using
 my $csv = Text::CSV->new();
+my $schema = dbase::Main->connect('dbi:SQLite:banker.db');
 
-my $schema = db::Main->connect('dbi:SQLite:banker.db');
 
+## This will come from the dbase eventually
 	my %regEx = (
 		"Sainsbusy", "food",
 		"WH Smith", "books",
@@ -21,10 +26,13 @@ my $schema = db::Main->connect('dbi:SQLite:banker.db');
 		"BOOKFAIR", "books",
 	);
 
+## These too, come from DB or form input
+## They tell us where abouts in the CSV file to look for data
 my $cDate = 0;
 my $cMatch = 1;
 my $cAmount = 2;
 
+## Setup the defaults
 my %totals = (
 		"Total Out", ("count", 0, "value" , 0),
 		"Total In", ("count", 0, "value" , 0),
@@ -32,6 +40,7 @@ my %totals = (
 
 open (CSV, "<", $file) or die $!;
 
+my @trans;
 while (<CSV>) {
 	if ($csv->parse($_)) {
 		my @columns = $csv->fields();
@@ -39,6 +48,7 @@ while (<CSV>) {
 		print "£".$columns[$cAmount]." - ";
 		while(my($exp, $label) = each(%regEx)){
 			if($columns[$cMatch] =~ m/$exp/){
+				push @trans, [$columns[$cMatch], $columns[$cAmount], $label, 1];
 				print $label;
 				$totals{$label}{'count'}++;
 				$totals{$label}{'value'} += $columns[ $cAmount ];
@@ -52,3 +62,9 @@ while (<CSV>) {
 	}
 }
 close CSV;
+
+## Put them al into the DBase
+$schema->populate('Tran', [
+		[qw/tran_text tran_amount tran_group tran_owner/],
+		@trans,
+	]);
